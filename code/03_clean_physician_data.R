68209cceea84d93afe126dadb8ea928a0cdf0bea
Beniamino Green <ben@greendalba.com>
2024-05-31 11:16:34 -0400

    Added Roxygen2 comments, added a missing library import

--- code/03_clean_physician_data.R
+++ code/03_clean_physician_data.R
@@ -1,40 +1,42 @@
-# Merge and Clean Physician Data
+#' Merge and Clean Physician Data
 #'
 #' @param cms_file path to CMS csv file which provides year of med school graduation per NPI
 #' @param nppes_file contains physician data including first name, last name, zip code
 #' @param nucc_file path to NUCC crosswalk file which provides a crosswalk between taxonomy codes and human-readable descriptions
+#'
+#' @return a consolidated dataframe which has the information from all three sources by NPI
 #'
 clean_physician_data <- function(cms_file, nppes_file, nucc_file) {
 	cms_data <- read_csv(cms_file) %>%
 	  rename_with(tolower) %>%
 		select(npi, grd_yr, med_sch) %>%
 		distinct()
-	
+
 	nppes_data <- read_csv(nppes_file) %>%
 		rename_with(~tolower(gsub(" ", "_", .x)))
-	
+
 	table(nppes_data$entity_type_code)
-	
-	subset_nppes_data <- nppes_data %>% 
+
+	subset_nppes_data <- nppes_data %>%
 		select(npi, provider_first_name, provider_middle_name, `provider_last_name_(legal_name)`,
-					 provider_business_mailing_address_state_name, provider_business_mailing_address_postal_code, 
-					 provider_business_mailing_address_state_name, healthcare_provider_taxonomy_code_1, 
+					 provider_business_mailing_address_state_name, provider_business_mailing_address_postal_code,
+					 provider_business_mailing_address_state_name, healthcare_provider_taxonomy_code_1,
 					 entity_type_code
 					 ) %>%
 		drop_na(npi) %>%
 		filter(entity_type_code == 1)
-	
-	nucc_data <- read_csv(nucc_file) %>% 
+
+	nucc_data <- read_csv(nucc_file) %>%
 		rename_with(~tolower(gsub(" ", "_", .x)))
-	
+
 	full_data <- left_join(subset_nppes_data, nucc_data, by = c("healthcare_provider_taxonomy_code_1" = "code"))  %>%
 		left_join(cms_data, by = "npi")
-	
-	full_data <- full_data %>% 
+
+	full_data <- full_data %>%
 		mutate(
 			physician = grouping == "Allopathic & Osteopathic Physicians"
 		) %>%
-		filter(physician) 
-	
+		filter(physician)
+
 	full_data
 }
--- code/06_random_forest.R
+++ code/06_random_forest.R
@@ -1,31 +1,47 @@
-library(tidyverse)
-library(grf)
-library(arrow)
-library(targets)
-
+#' Make Predictor Matrix
+#'
+#' @description Extract the predictors used for a classification model, and format them as a
+#' numeric matrix
+#'
+#' @param df the input dataframe
+#'
+#' @return a numeric predictor matrix
+#'
 make_X_matrix <- function(df) {
-	df <- df %>% 
-		mutate(med_prof = grepl("Medical", CommercialData_Occupation)) %>% 
+	df <- df %>%
+		mutate(med_prof = grepl("Medical", CommercialData_Occupation)) %>%
 		select(zip_dist, year_dist, full_name_sim, mid_initial_agree, mid_name_agree, n)
-	
+
 	 model.matrix.lm(~-1 + ., df, na.action=na.pass)
 }
 
 
+#' Add Random Forest Matching Predictions to the LSH dataframe
+#'
+#' @description Fit a Random Forest model to predict whether pairs of records
+#' match based on labelled training data. Use this Random Forest to predict
+#' whether pairs of records given by LSH are matches.
+#'
+#' @param lshed_data dataframe of potential matches created by LSH functions
+#' @param labelled_training_files paths to the labelled training data
+#'
+#' @return The input dataframe, with an extra column given the probability that
+#' each pair of records matches, as predicted by the random forest
+#'
 add_rf_match_predictions_to_df <- function(labelled_training_files, lshed_data){
-	labelled_training_data <- labelled_training_files %>% 
+	labelled_training_data <- labelled_training_files %>%
 		map_df(read_parquet)
-	
+
 	training_X <- make_X_matrix(labelled_training_data)
 	training_Y <- as.factor(labelled_training_data$match)
-	
+
 	model <- probability_forest(training_X, training_Y)
-	
-	
+
+
 	X <- make_X_matrix(lshed_data)
 	lshed_data$match <- predict(model, newdata=X)$predictions[,2]
-	
-	return(lshed_data)	
+
+	return(lshed_data)
 }
 
 
